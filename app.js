448a328b346b05e7979c74cd5d4388729f7db566
DYLANeay <dylan.eray6@gmail.com>
2025-07-22 13:59:01 +0200

    query params example

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const express = require('express');
 const app = express();
 
 //Route
-/* app.get('/', (req, res) => {
+app.get('/', (req, res) => {
   // Can only use res.send() once per request
   res.send('Hello, World!!');
-}); */
+});
 
 //Route with a parameter, {*whatever} if I want a placeholder for any value
 app.get('/:username/', (req, res) => {
   console.log(req.params); // { username: 'dylan' }
+  console.log(req.query); // { query: 'value' } if there are query parameters (?)
   res.send(`Hello, ${req.params.username}!`);
 });
 
