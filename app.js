0185dbd0568ca7ec839aeb75ee4f3fa0379dd1e2
DYLANeay <dylan.eray6@gmail.com>
2025-07-22 13:34:54 +0200

    comments

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const app = express();
 
-//route
+//Route
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  // Can only use res.send() once per request
+  res.send('Hello, World!!');
 });
 
 const PORT = process.env.PORT || 3000;
